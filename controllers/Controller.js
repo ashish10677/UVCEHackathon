389fcd9bd2f115acb6e0b303e152748c0bc60fae
Glitch (women) <none>
2018-09-22 06:51:23 +0000

    🎒⏱ Checkpoint ./controllers/Controller.js:1565514/2846 ./.trigger-rebuild:1534710/28 ./routes/forum-routes.js:1534710/33 ./landing.html:1565514/4490

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -24,6 +24,34 @@ module.exports=function(app){
         // });
     });
     
+    //post request to verify number
+    app.post('/verify_phone',jsonParser,function(req,res){
+        //get data from mongodb and pass it to view
+        const client = require('twilio')(
+          "AC53fbc5bf2752730f60e585c315ccdaa4","780cdcb2d905b40c91b9719b12f80b47"
+        );
+        client.validationRequests
+        .create({
+           friendlyName: 'My Home Phone Number',
+           phoneNumber: '+91'+req.body.phone
+         })
+        .then(validation_request => console.log(validation_request.validationCode))
+        .done();
+    });  
+  
+    //post request to send sms
+    app.post('/send_sms',jsonParser,function(req,res){
+        //get data from mongodb and pass it to view
+        const client = require('twilio')(
+          "AC53fbc5bf2752730f60e585c315ccdaa4","780cdcb2d905b40c91b9719b12f80b47"
+        );
+        client.messages.create({
+          from: "+15707225182",
+          to: '+91'+req.body.phone,
+          body: "http://www.google.com/maps/place/12.9748561,77.58618249999999"
+        }).then(message => console.log(message.sid));
+    }); 
+  
     //post request to retrieve profile
     app.post('/retrieve_profile',jsonParser,function(req,res){
         //get data from mongodb and pass it to view
--- routes/forum-routes.js
+++ routes/forum-routes.js
@@ -3,8 +3,6 @@ const Feed = require('../models/feed-model');
 
 var Samplefeed={hi:"d"}
 router.get('/', (req, res) => {
-  res.json(Samplefeed);
-  return
   Feed.find({},function (err, feed) {
 			if (err) return res.status(500).send({ error: err });
       // res.render('forum',{feed})
