75d81ad999fb22e13b335f4485a69bfa0697a58b
Glitch (women) <none>
2018-09-22 06:19:18 +0000

    💦🎀 Checkpoint ./routes/forum-routes.js:1534710/436

--- routes/forum-routes.js
+++ routes/forum-routes.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 const Feed = require('../models/feed-model');
 
+
+
 router.get('/', (req, res) => {
-    res.render('forum',{})
+  Feed.find({},function (err, feed) {
+			if (err) return res.status(500).send({ error: err });
+      res.render('forum',{feed})
+			res.json(feed);
+})
+    
 });
 
 module.exports = router;
