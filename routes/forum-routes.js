1ec6b94e2a45ba29173c55156828db13a283260f
Glitch (women) <none>
2018-09-22 07:12:23 +0000

    🍧🎋 Checkpoint ./views/profile.ejs:1565514/9027 ./routes/forum-routes.js:1534710/299 ./views/forum.ejs:1534710/203 ./.trigger-rebuild:1534710/28

--- routes/forum-routes.js
+++ routes/forum-routes.js
@@ -8,12 +8,24 @@ var Samplefeed={
       title:"title",
       body:"testbody",
       comments:[]
+    },
+    {
+      username:"testuser2",
+      title:"title2",
+      body:"testbody2",
+      comments:[]
+    },
+    {
+      username:"testuser3",
+      title:"title3",
+      body:"testbody3",
+      comments:[]
     }
   ]
   
 }
 router.get('/', (req, res) => {
-  Feed.find({},function (err, feed) {
+  Feed.find({},{'_id':0,'__v':0}, function (err, feed) {
 			if (err) return res.status(500).send({ error: err });
       res.render('forum',{feed:Samplefeed})
 			// res.json(feed);
